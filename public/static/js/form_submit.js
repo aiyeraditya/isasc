67d6dbbfc7e9395d7c88e670f4b8ad9bc7d8fd36
Aditya Iyer <aiyer.aditya@gmail.com>
2021-12-04 12:24:10 +0100

    New Content From Django
    Major Overhaul

--- public/static/js/form_submit.js
+++ public/static/js/form_submit.js
@@ -1,50 +1,50 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBSDtGDFk_6DHO5rpxpkWY3xtvy1gGRUkE",
-    authDomain: "isasc-f26c0.firebaseapp.com",
-    databaseURL: "https://isasc-f26c0-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "isasc-f26c0",
-    storageBucket: "isasc-f26c0.appspot.com",
-    messagingSenderId: "777797932718",
-    appId: "1:777797932718:web:d054dbcf09b6db6070bf3f",
-    measurementId: "G-WK9Z0LZCDD"
-  };
+  apiKey: "AIzaSyBSDtGDFk_6DHO5rpxpkWY3xtvy1gGRUkE",
+  authDomain: "isasc-f26c0.firebaseapp.com",
+  databaseURL: "https://isasc-f26c0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "isasc-f26c0",
+  storageBucket: "isasc-f26c0.appspot.com",
+  messagingSenderId: "777797932718",
+  appId: "1:777797932718:web:d054dbcf09b6db6070bf3f",
+  measurementId: "G-WK9Z0LZCDD"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  // Reference messages collection
-  var messagesRef = firebase.database().ref('messages');
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('join-form').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('InputName');
+  var email = getInputVal('InputEmail1');
   
-  // Listen for form submit
-  document.getElementById('join-form').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
-    e.preventDefault();
-  
-    // Get values
-    var name = getInputVal('InputName');
-    var email = getInputVal('InputEmail1');
-    
-    console.log(name);
-  
-    // Save message
-    saveMessage(name, email);
-  
-    // Clear form and Redirect to Thanks page
-    document.getElementById('join-form').reset();
-    window.location.replace("/thanks");
-  }
-  
-  // Function to get get form values
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage(name, email){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      name: name,
-      email:email,
-    });
-  }
+  console.log(name);
+
+  // Save message
+  saveMessage(name, email);
+
+  // Clear form and Redirect to Thanks page
+  document.getElementById('join-form').reset();
+  window.location.replace("/thanks");
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+  });
+}
